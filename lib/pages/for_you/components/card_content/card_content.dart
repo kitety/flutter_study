581f130cadc67ecd5ebf9d05e9007ba3c7524c34
kitety <aizaizuori@foxmail.com>
2024-03-22 23:00:48 +0800

    feat: card move animation

--- lib/pages/for_you/components/card_content/card_content.dart
+++ lib/pages/for_you/components/card_content/card_content.dart
@@ -1,47 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_study/pages/for_you/components/card_content/animate_bottom_btn.dart';
+import 'package:flutter_study/pages/for_you/components/card_content/animate_center_img.dart';
+import 'package:flutter_study/pages/for_you/components/card_content/constant.dart';
 
 class CardContent extends StatefulWidget {
-  int index;
-  CardContent({Key? key, required this.index}) : super(key: key);
+  final int index;
+  const CardContent({Key? key, required this.index}) : super(key: key);
 
   @override
   _CardContentState createState() => _CardContentState();
 }
 
-class _CardContentState extends State<CardContent> {
+class _CardContentState extends State<CardContent>
+    with TickerProviderStateMixin {
+  List<AnimationController> btnControllers = [];
+  List<Function> heartControllers = [];
+  late final AnimationController _controller = AnimationController(
+    duration: cardScaleDuration,
+    vsync: this,
+  );
+
   @override
   Widget build(BuildContext context) {
     const likeImage = AssetImage('images/like_btn.png');
     const chatImage = AssetImage('images/chat.png');
-    return Stack(
-      children: [
-        Container(
-          width: double.infinity,
-          color: Colors.black12,
-          child: Image(
-            image: NetworkImage(
-              'https://picsum.photos/200/300?random=${widget.index}',
-            ),
-            fit: BoxFit.contain,
-          ),
-        ),
-        const Positioned(
-          bottom: 20,
-          right: 20,
-          child: Row(
-            children: [
-              AnimateBottomBtn(
-                  child: Image(image: chatImage, width: 50, height: 50)),
-              SizedBox(
-                width: 20,
+    return Container(
+      child: ScaleTransition(
+        scale: Tween<double>(
+          begin: 1.0, // 原始宽度
+          end: 0.95, // 结束宽度（原始宽度的0.85倍）
+        ).animate(_controller),
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              color: Colors.black12,
+              child: Image(
+                image: NetworkImage(
+                  'https://picsum.photos/200/300?random=${widget.index}',
+                ),
+                fit: BoxFit.contain,
               ),
-              AnimateBottomBtn(
-                  child: Image(image: likeImage, width: 50, height: 50))
-            ],
-          ),
-        )
-      ],
+            ),
+            Positioned(
+              bottom: 20,
+              right: 20,
+              child: GestureDetector(
+                onTap: handleBottomBtnTap,
+                child: Row(
+                  children: [
+                    AnimateBottomBtn(
+                      controllers: btnControllers,
+                      child:
+                          const Image(image: chatImage, width: 50, height: 50),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    AnimateBottomBtn(
+                      controllers: btnControllers,
+                      child:
+                          const Image(image: likeImage, width: 50, height: 50),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Opacity(
+              // duration: cardScaleDuration,
+              opacity: Tween<double>(
+                begin: 1.0,
+                end: 0.95,
+              ).animate(_controller).value,
+              child: AnimateCenterImg(controllers: heartControllers),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void handleBottomBtnTap() async {
+    for (var controller in btnControllers) {
+      controller.reverse();
+    }
+    await Future.delayed(const Duration(milliseconds: bottomBrnShowTime));
+    for (var controller in heartControllers) {
+      controller.call();
+    }
+    await Future.delayed(const Duration(milliseconds: heartDurationTime));
+    _controller.forward();
+    await Future.delayed(const Duration(milliseconds: cardScaleTime));
   }
 }
--- lib/pages/for_you/components/card_content/constant.dart
+++ lib/pages/for_you/components/card_content/constant.dart
@@ -1,1 +1,11 @@
-const bottomBtnShow=Duration(milliseconds: 1500);
+const bottomBrnShowTime = 500;
+const bottomBtnShowTime = Duration(milliseconds: bottomBrnShowTime);
+
+const heartDuration = Duration(milliseconds: heartDurationTime);
+const heartDurationTime = 600;
+
+const cardScaleTime=400;
+const cardScaleDuration = Duration(milliseconds: cardScaleTime);
+
+const cardTranslateTime=400;
+const cardTranslateDuration = Duration(milliseconds: cardTranslateTime);
